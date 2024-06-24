70649f3dfe9eade35c4b8b97db9463541c28d30d
lesterc013 <lesterc013@gmail.com>
2024-06-24 11:53:42 +0800

    uibuild

--- index.js
+++ index.js
@@ -111,6 +111,7 @@ app.post('/api/persons', (request, response) => {
    response.json(phonebook)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log('Server running on port', PORT)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log('Server running on port', PORT)
+})
